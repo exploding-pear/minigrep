fd37b5d770b382ed53c97d5a671cceb56215df54
exploding-pear <alexjdupper@gmail.com>
2020-05-16 13:35:55 -0400

    printing errors to stderr now

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ fn main() {
     let args : Vec<String> = env::args().collect();
 
     let configs = Config::new(&args).unwrap_or_else(|err| {
-        println!("problem passing arguments: {}", err);
+        eprintln!("problem passing arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(&configs) {
-        println!("application error: {}", e);
+        eprintln!("application error: {}", e);
         process::exit(1);
     }
 }
