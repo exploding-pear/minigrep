9974fbcc661382cd7b1413af8e2c963b5a38f975
exploding-pear <alexjdupper@gmail.com>
2020-05-16 13:01:59 -0400

    added comments about case insensitive check in search()

--- src/lib.rs
+++ src/lib.rs
@@ -35,6 +35,7 @@ fn search(config: &Config, case_insenstive: bool) -> Result<Vec<String>, Box<dyn
     let mut reader = BufReader::new(f);
     let mut line = String::new();
     let mut val : usize;
+    //creating a lowercase query for a case insensitive search
     let insensitive_query = config.query.to_lowercase();
     let mut insensitive_line;
 
@@ -43,6 +44,7 @@ fn search(config: &Config, case_insenstive: bool) -> Result<Vec<String>, Box<dyn
         if val == 0 {
             break
         }
+        //case insensitive check. line being checked and query are both lowercased and compared
         else if case_insenstive == true {
             insensitive_line = line.to_lowercase();
             if insensitive_line.contains(& insensitive_query) {
@@ -50,6 +52,7 @@ fn search(config: &Config, case_insenstive: bool) -> Result<Vec<String>, Box<dyn
             }
             insensitive_line.clear();
         }
+        //case sensitive check. compares line to query
         else if line.contains(config.query) {
             results.push(line.clone());
         }
